7c4bc34d3c8379cff80a3ca953f97bf996aaa6a5
Ólafur Ingi Kárason <oikarason@gmail.com>
2023-09-10 21:50:40 +0000

    init bruv

---
+++ src/main/java/com/eplbetting/eplbetting/EplbettingApplication.java
@@ -0,0 +1,13 @@
+package com.eplbetting.eplbetting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EplbettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EplbettingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eplbetting/eplbetting/EplbettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eplbetting.eplbetting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EplbettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
